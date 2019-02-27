00c3343235258c3833c52f335bfa0207c7589978
iwantmyfile5 <iwantmyfile5@users.noreply.github.com>
2019-02-27 15:03:38 -0500

    Chunk Building
    Created script to build chunk. Optimized rendering by only randering quads on block faces that have a non-solid block adjacent to them.

--- VoxelWorld/Assets/Block.cs
+++ VoxelWorld/Assets/Block.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateQuads : MonoBehaviour {
+public class Block {
 
 	enum Cubeside {BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK};
 	public enum BlockType {GRASS, DIRT, STONE};
 
-	public Material cubeMaterial;
-	public BlockType bType;
+	BlockType bType;
+	public bool isSolid;
+	GameObject parent;
+	Vector3 position;
+	Material cubeMaterial;
 
 	Vector2[,] blockUVs = { 
 		/*GRASS TOP*/		{new Vector2( 0.125f, 0.375f ), new Vector2( 0.1875f, 0.375f),
@@ -20,6 +23,15 @@ public class CreateQuads : MonoBehaviour {
 		/*STONE*/			{new Vector2( 0, 0.875f ), new Vector2( 0.0625f, 0.875f),
 								new Vector2( 0, 0.9375f ),new Vector2( 0.0625f, 0.9375f )}
 						}; 
+
+	public Block(BlockType b, Vector3 pos, GameObject p, Material c)
+	{
+		bType = b;
+		parent = p;
+		position = pos;
+		cubeMaterial = c;
+		isSolid = true;
+	}
 
 	void CreateQuad(Cubeside side)
 	{
@@ -123,62 +135,42 @@ public class CreateQuads : MonoBehaviour {
 		mesh.RecalculateBounds();
 		
 		GameObject quad = new GameObject("Quad");
-	    quad.transform.parent = this.gameObject.transform;
+		quad.transform.position = position;
+	    quad.transform.parent = parent.transform;
+
      	MeshFilter meshFilter = (MeshFilter) quad.AddComponent(typeof(MeshFilter));
 		meshFilter.mesh = mesh;
+
 		MeshRenderer renderer = quad.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
 		renderer.material = cubeMaterial;
 	}
 
-	void CombineQuads()
+	public bool HasSolidNeighbour(int x, int y, int z)
+	{
+		Block[,,] chunks = parent.GetComponent<Chunk>().chunkData;
+		try
+		{
+			return chunks[x,y,z].isSolid;
+		}
+		catch(System.IndexOutOfRangeException ex){}
+
+		return false;
+	}
+
+	public void Draw()
 	{
 		
-		//1. Combine all children meshes
-		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
-        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
-        int i = 0;
-        while (i < meshFilters.Length) {
-            combine[i].mesh = meshFilters[i].sharedMesh;
-            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-            i++;
-        }
-
-        //2. Create a new mesh on the parent object
-        MeshFilter mf = (MeshFilter) this.gameObject.AddComponent(typeof(MeshFilter));
-        mf.mesh = new Mesh();
-
-        //3. Add combined meshes on children as the parent's mesh
-        mf.mesh.CombineMeshes(combine);
-
-        //4. Create a renderer for the parent
-		MeshRenderer renderer = this.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
-		renderer.material = cubeMaterial;
-
-		//5. Delete all uncombined children
-		foreach (Transform quad in this.transform) {
-     		Destroy(quad.gameObject);
- 		}
-
-	}
-
-	void CreateCube()
-	{
-		CreateQuad(Cubeside.FRONT);
-		CreateQuad(Cubeside.BACK);
-		CreateQuad(Cubeside.TOP);
-		CreateQuad(Cubeside.BOTTOM);
-		CreateQuad(Cubeside.LEFT);
-		CreateQuad(Cubeside.RIGHT);
-		CombineQuads();
-	}
-
-	// Use this for initialization
-	void Start () {
-		CreateCube();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		if(!HasSolidNeighbour((int)position.x,(int)position.y,(int)position.z + 1))
+			CreateQuad(Cubeside.FRONT);
+		if(!HasSolidNeighbour((int)position.x,(int)position.y,(int)position.z - 1))
+			CreateQuad(Cubeside.BACK);
+		if(!HasSolidNeighbour((int)position.x,(int)position.y + 1,(int)position.z))
+			CreateQuad(Cubeside.TOP);
+		if(!HasSolidNeighbour((int)position.x,(int)position.y - 1,(int)position.z))
+			CreateQuad(Cubeside.BOTTOM);
+		if(!HasSolidNeighbour((int)position.x - 1,(int)position.y,(int)position.z))
+			CreateQuad(Cubeside.LEFT);
+		if(!HasSolidNeighbour((int)position.x + 1,(int)position.y,(int)position.z))
+			CreateQuad(Cubeside.RIGHT);
 	}
 }
